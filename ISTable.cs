eeecc700a13900b73046f1cdd0723ea6109e7c1d
lsaltz <40012441+lsaltz@users.noreply.github.com>
2019-12-03 21:13:31 -0700

    Add files via upload

---
+++ ISTable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SQLite;
+
+namespace ScoutingApp1
+{
+    class ISTable
+    {
+        [PrimaryKey, AutoIncrement, Column("Id")]
+
+        public int id { get; set; }
+        public int Score { get; set; }
+        public int Team { get; set; }
+        public byte[] Image { get; set; }
+    }
+}
---
+++ ScoresViewAdapter.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace ScoutingApp1
+{
+    class ScoresViewAdapter : BaseAdapter<ScoresTable>
+    {
+        public List<ScoresTable> iitems;
+        public Activity icontext;
+
+
+
+        public ScoresViewAdapter(Activity context, List<ScoresTable> items)
+        {
+            iitems = items;
+            icontext = context;
+
+        }
+        public override int Count
+        {
+
+            get { return iitems.Count; }
+
+        }
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public override ScoresTable this[int position]
+        {
+            get { return iitems[position]; }
+        }
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+
+            var row = convertView;
+
+            if (row == null)
+            {
+
+                row = LayoutInflater.From(icontext).Inflate(Resource.Layout.truescores_list, null);
+            }
+            try
+            {
+
+
+
+                var mnumber = row.FindViewById<TextView>(Resource.Id.txtView_STeam);
+                string number = iitems[position].Team.ToString();
+                mnumber.Text = number;
+
+                var totalScore = row.FindViewById<TextView>(Resource.Id.txtView_TotalScore);
+                string scoreN = iitems[position].TotalScore.ToString();
+                totalScore.Text = scoreN;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            return row;
+
+
+        }
+    }
+}
