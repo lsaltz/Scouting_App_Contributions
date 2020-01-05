813b0c07e443fdc3980f0d6bbf30dab9e4b1361c
lsaltz <40012441+lsaltz@users.noreply.github.com>
2020-01-04 17:35:30 -0700

    no message

---
+++ ScoutingApp1/obj/Debug/90/android/typemap.jm.arm64-v8a.s
@@ -0,0 +1,26 @@
+	/* Data SHA1: f6052c6a5f0d212683d3b415bf32d1110fe92b30 */
+	.arch	armv8-a
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.word	1
+	/* entry-count */
+	.word	1042
+	/* entry-length */
+	.word	262
+	/* value-offset */
+	.word	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 273005
+	.include	"typemap.jm.inc"
