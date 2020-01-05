4fdca7e815b0fb7937113f482d30a2111665870a
lsaltz <40012441+lsaltz@users.noreply.github.com>
2020-01-04 17:33:43 -0700

    draft 3 up

---
+++ Draft3/obj/Debug/90/android/typemap.mj.x86.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: 71ac5f5ae0bfe7daab48ba32cd61a29bb2f80ee9 */
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1148
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 300777
+	.include	"typemap.mj.inc"
